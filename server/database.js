e80f87f8a9173635525f23ca5b2e5212240628ae
akash5100 <shivamvermashs@gmail.com>
2022-07-12 20:30:48 +0530

    Created new tables

--- server/database.js
+++ server/database.js
@@ -1,5 +1,4 @@
 var sqlite3 = require("sqlite3").verbose();
-var md5 = require("md5");
 
 const DBSOURCE = "db.sqlite";
 
@@ -9,21 +8,103 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     throw err;
   } else {
     console.log("Connected to the database.");
-    db.run(`CREATE TABLE IF NOT EXISTS user (
+    db.serialize(() => {
+      db.run(`CREATE TABLE IF NOT EXISTS user (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name text,
+              email text UNIQUE,
+              password text,
+              CONSTRAINT email_unique UNIQUE (email)
+            )`),
+          (err) => {
+            if (err) {
+              console.error(err.message);
+              throw err;
+            } else {
+              console.log("User table created.");
+            }
+          };
+      db.run(`CREATE TABLE IF NOT EXISTS os (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              ct1 INTEGER,
+              ct2 INTEGER,
+              final INTEGER,
+              user_id INTEGER,
+              FOREIGN KEY (user_id) REFERENCES user(id)
+            )`),
+          (err) => {
+            if (err) {
+              console.error(err.message);
+              throw err;
+            } else {
+              console.log("os table created.");
+            }
+          }
+      db.run(`CREATE TABLE IF NOT EXISTS dbms (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text,
-            email text UNIQUE,
-            password text,
-            CONSTRAINT email_unique UNIQUE (email)
-        )`),
-      (err) => {
-        if (err) {
-          console.error(err.message);
-          throw err;
-        } else {
-          console.log("Table created.");
+            ct1 INTEGER,
+            ct2 INTEGER,
+            final INTEGER,
+            user_id INTEGER,
+            FOREIGN KEY (user_id) REFERENCES user(id)
+          )`),
+        (err) => {
+          if (err) {
+            console.error(err.message);
+            throw err;
+          } else {
+            console.log("dbms table created.");
+          }
         }
-      };
+      db.run(`CREATE TABLE IF NOT EXISTS dsa(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ct1 INTEGER,
+            ct2 INTEGER,
+            final INTEGER,
+            user_id INTEGER,
+            FOREIGN KEY (user_id) REFERENCES user(id)
+          )`),
+        (err) => {
+          if (err) {
+            console.error(err.message);
+            throw err;
+          } else {
+            console.log("dsa table created.");
+          }
+        }
+      db.run(`CREATE TABLE IF NOT EXISTS cn(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ct1 INTEGER,
+            ct2 INTEGER,
+            final INTEGER,
+            user_id INTEGER,
+            FOREIGN KEY (user_id) REFERENCES user(id)
+          )`),
+        (err) => {
+          if (err) {
+            console.error(err.message);
+            throw err;
+          } else {
+            console.log("cn table created.");
+          }
+        }
+      db.run(`CREATE TABLE IF NOT EXISTS plang(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ct1 INTEGER,
+            ct2 INTEGER,
+            final INTEGER,
+            user_id INTEGER,
+            FOREIGN KEY (user_id) REFERENCES user(id)
+          )`),
+        (err) => {
+          if (err) {
+            console.error(err.message);
+            throw err;
+          } else {
+            console.log("plang table created.");
+          }
+        }
+    });
   }
 });
 
