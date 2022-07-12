467e784a97ca21374f41d0ab3f61ec595e09c9b4
akash5100 <shivamvermashs@gmail.com>
2022-07-12 20:45:47 +0530

    adds utility module

---
+++ server/utils.js
@@ -0,0 +1,17 @@
+export const validateRequest = (req, res) => {
+    var error = [];
+    if (!req.body.name) {
+      error.push("Name is required");
+    }
+    if (!req.body.password) {
+      error.push("Password is required");
+    }
+    if (!req.body.email) {
+      error.push("Email is required");
+    }
+    if (error.length) {
+      res.status(400).json({ error: error.join(", ") });
+      return false;
+    }
+    return true;
+  };
